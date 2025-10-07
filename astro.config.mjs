692dac1f4fbe869d861dd80cb51743d88f18538a
percyq19 <63437612+percyq19@users.noreply.github.com>
2025-10-07 18:37:17 -0500

    mipost2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 // Astro config, you likely won't need to change this file. But if you do -> https://docs.astro.build/en/reference/configuration-reference/
-import { SITE } from "./src/alkaline.config";
 import robotsConfig from "./src/utils/robots-txt.config";
 
 import { defineConfig } from "astro/config";
